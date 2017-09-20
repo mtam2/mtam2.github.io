09dcc602e2a84f02a63c2df1afc6e2c408cf341f
Maxim Tam <maximtea.cloud@gmail.com>
2017-09-20 10:33:45 -0400

    Corrected Jekyll build error?

--- js/data.js
+++ js/data.js
@@ -504,7 +504,6 @@ var yolo = `
     <ol class="carousel-indicators">
         <li data-target="#carousel" data-slide-to="0" class="active"></li>
         <li data-target="#carousel" data-slide-to="1"></li>
-        <li data-target="#carousel" data-slide-to="2"></li>
     </ol>
     <!--/.Indicators-->
     <!--Slides-->
